63b8afbbe27adc1d4c4816c988beb8ad80e00a3d
Anandha <anandha@truweight.in>
2019-05-10 15:49:11 +0530

    missing files

---
+++ app/src/main/java/com/sastra/im037/sastraprakasika/mediareceiver/NetworkStateReceiverListener.java
@@ -0,0 +1,6 @@
+package com.sastra.im037.sastraprakasika.mediareceiver;
+
+public interface NetworkStateReceiverListener {
+    public void networkAvailable();
+    public void networkUnavailable();
+}
---
+++ app/src/main/java/com/sastra/im037/sastraprakasika/mediaservice/ConnectivityReceiver.java
@@ -0,0 +1,69 @@
+package com.sastra.im037.sastraprakasika.mediaservice;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import com.sastra.im037.sastraprakasika.mediareceiver.NetworkStateReceiverListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConnectivityReceiver extends BroadcastReceiver {
+
+    protected List<NetworkStateReceiverListener> listeners;
+    protected Boolean connected;
+
+    public ConnectivityReceiver() {
+        listeners = new ArrayList<NetworkStateReceiverListener>();
+        connected = null;
+    }
+
+
+    public void onReceive(Context context, Intent intent) {
+        if(intent == null || intent.getExtras() == null)
+            return;
+
+        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo ni = manager.getActiveNetworkInfo();
+
+        if(ni != null && ni.getState() == NetworkInfo.State.CONNECTED) {
+            connected = true;
+        } else if(intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY,Boolean.FALSE)) {
+            connected = false;
+        }
+
+        notifyStateToAll();
+    }
+
+    private void notifyStateToAll() {
+        for(NetworkStateReceiverListener listener : listeners)
+            notifyState(listener);
+    }
+
+    private void notifyState(NetworkStateReceiverListener listener) {
+        if(connected == null || listener == null)
+            return;
+
+        if(connected == true)
+            listener.networkAvailable();
+        else
+            listener.networkUnavailable();
+    }
+
+    public void addListener(NetworkStateReceiverListener l) {
+        listeners.add(l);
+        notifyState(l);
+    }
+
+    public void removeListener(NetworkStateReceiverListener l) {
+        listeners.remove(l);
+    }
+
+//    public interface NetworkStateReceiverListener {
+//        public void networkAvailable();
+//        public void networkUnavailable();
+//    }
+}
